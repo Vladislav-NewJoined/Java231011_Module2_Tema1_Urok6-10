349d0921f2d203ce88c440b337a3668c6c0957b0
Vladislav-NewJoined <sozin.vladislav@mail.ru>
2023-10-11 21:35:15 +0300

    Initial commit

--- src/Task2_1_6_1.java
+++ src/Task2_1_6_1.java
@@ -1,5 +1,31 @@
-public class Main {
+import java.util.ArrayList;
+
+public class Task2_1_6_1 {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("""
+                Задание:\s
+                Урок 6. Перебор и группировка (map).
+                    Задание: Необходимо вывести большими буквами названия всех продуктов из магазина 
+                    электроники, написанного на уроке.
+
+                Решение:\s""");
+
+        ArrayList<Product> catalog = new ArrayList<>();
+
+        catalog.add(new Product("Nokia 3310", 12345676));
+        catalog.add(new Product("Samsung Galaxy S100", 50304));
+        catalog.add(new Product("IPhone 20", 392049));
+        catalog.add(new Product("Google Pixel 10a", 203421));
+        catalog.stream().map((product -> product.name)).forEach(x -> System.out.println(x.toUpperCase()));
+    }
+}
+
+class Product {
+    String name;
+    int price;
+
+    public Product(String name, int price) {
+        this.name = name;
+        this.price = price;
     }
 }

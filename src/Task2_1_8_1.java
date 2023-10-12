68834b9e926fa7659a137be7fb93cf10a9365ffd
Vladislav-NewJoined <sozin.vladislav@mail.ru>
2023-10-12 15:08:57 +0300

    Initial commit

---
+++ src/Task2_1_8_1.java
@@ -0,0 +1,16 @@
+import java.util.stream.IntStream;
+
+public class Task2_1_8_1 {
+    public static void main(String[] args) {
+        System.out.println("""
+                Задание:\s
+                Урок 8. Optional.
+                    Задание: Вам необходимо из массива “-10 –10” взять числа меньше 10, которые 
+                    получатся в результате функции x2 –5x –10 и вывести квадрат максимального.
+
+                Решение:\s""");
+
+        IntStream.range(-10, 10).map(x -> x * x - 5 * x - 10).filter(x -> x < 10).max().ifPresentOrElse((n) ->
+                System.out.println(n * n), () -> System.out.println("Число, удовлетворяющее условиям, не найдено"));
+    }
+}
